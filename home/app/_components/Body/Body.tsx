deb971bfdbe23577d50cd1dc427d9be716aa7b18
Tunahan İPEK <tnhnipek@gmail.com>
2024-08-17 15:10:50 +0300

    fix page

--- home/app/_components/Body/Body.tsx
+++ home/app/_components/Body/Body.tsx
@@ -1,13 +1,11 @@
-import Button from '@/app/_ui/Button';
-import Image from 'next/image';
+import Button from "@/app/_ui/Button";
+import Image from "next/image";
 
 const Body = () => {
   return (
-    <main
-      className="flex flex-wrap justify-center w-full h-auto px-5 bg-gray-200 shadow-lg rounded-tl-3xl rounded-tr-[400px] rounded-br-[200px] rounded-bl-[600px]"
-    >
-      <div className="flex flex-col">
-        <div className="duration-200 transform rounded-t-full shadow-lg bg-yellow-50 easy-in-out">
+    <main className="flex flex-wrap justify-center w-full h-[calc(100vh-180px)] px-5 bg-gray-200 shadow-lg rounded-tl-3xl rounded-tr-[400px] rounded-br-[200px] rounded-bl-[600px]">
+      <div className="flex flex-col h-full">
+        <div className="duration-200 transform rounded-t-full shadow-lg bg-yellow-50 easy-in-out h-full">
           <article className="max-w-[1500px] w-full h-40 mx-auto max-2xl:px-10 max-2xl:rounded-full overflow-hidden rounded-xl">
             <Image
               width={1500}
@@ -20,17 +18,15 @@ const Body = () => {
           <article className="flex justify-center px-5 -mt-12">
             <Image
               className="w-40 h-40 p-2 bg-green-50 rounded-[70%_30%_30%_70%/60%_40%_60%_40%] opacity-profil"
-              src='/tunahanipek.jpg'
+              src="/tunahanipek.jpg"
               alt="Profil Fotoğrafı"
               width="1920"
               height="1080"
             />
           </article>
-          <div className="flex flex-col">
-            <header className="text-center px-14 max-sm:px-5">
-              <h2 className="text-3xl font-bold text-gray-800">
-                Tunahan İPEK
-              </h2>
+          <div className="relative flex flex-col h-[calc(70vh-200px)]">
+            <header className="flex flex-col justify-center items-center text-center px-14 max-sm:px-5 h-full">
+              <h2 className="text-3xl font-bold text-gray-800">Tunahan İPEK</h2>
               <p className="mt-2 text-gray-400">KİŞİSEL WEB SİTESİ</p>
               <p className="mt-1 text-gray-400">
                 - kurumsal olma yolunda ilk adımlar -
@@ -38,28 +34,29 @@ const Body = () => {
               <br />
               <p className="mt-2 text-gray-600">
                 Merhabalar ben Tunahan, kısaca kendimi tanıtmam gerekirse 24
-                yaşındayım ve Denizli&apos;de yaşıyorum. Isparta&apos;da Biyomedikal
-                bölümünden mezun oldum. Yazılım ile hobi olarak ilgileniyorken bu işi meslek olarak yapmaya karar verdim. Şu anda Rubiklabs&apos;da Software Developer olarak çalışıyorum. Aynı zamanda kendi blog sitemde yazılım ile ilgili yazılar yazıyorum. Bu siteyi kurma amacım ise bana ulaşmak isteyenlere yol göstermek ve bilgilendirmek. Sorularınız için mail adreslerimi aşağıya bırakıyorum. Benim hakkımda daha fazla bilgi almak için lüfen Blog sitemi ziyaret et..
+                yaşındayım ve Denizli&apos;de yaşıyorum. Isparta&apos;da
+                Biyomedikal bölümünden mezun oldum. Yazılım ile hobi olarak
+                ilgileniyorken bu işi meslek olarak yapmaya karar verdim. Şu
+                anda Rubiklabs&apos;da Software Developer olarak çalışıyorum.
+                Aynı zamanda kendi blog sitemde yazılım ile ilgili yazılar
+                yazıyorum. Bu siteyi kurma amacım ise bana ulaşmak isteyenlere
+                yol göstermek ve bilgilendirmek. Sorularınız için mail
+                adreslerimi aşağıya bırakıyorum. Benim hakkımda daha fazla bilgi
+                almak için lüfen Blog sitemi ziyaret et..
               </p>
             </header>
             <hr className="mt-6" />
-            <div
-              className="flex rounded-t-full max-sm:rounded-t-none bg-gray-50 max-sm:flex-col"
-            >
+            <div className="flex rounded-t-full max-sm:rounded-t-none bg-gray-50 max-sm:flex-col">
               <div className="w-1/2 p-4 text-center cursor-default hover:bg-gray-100 max-sm:w-full">
                 <p className="flex flex-row justify-center max-md:flex-col max-sm:text-sm">
-                  <span className="font-semibold mr-1">
-                    KURUMSAL E-MAIL
-                  </span>
+                  <span className="font-semibold mr-1">KURUMSAL E-MAIL</span>
                   hello@tunahanipek.com
                 </p>
               </div>
               <div className="border"></div>
               <div className="w-1/2 p-4 text-center cursor-default hover:bg-gray-100 max-sm:w-full">
                 <p className="flex flex-row justify-center max-md:flex-col max-sm:text-sm">
-                  <span className="font-semibold mr-1">
-                    KİŞİSEL E-MAIL
-                  </span>
+                  <span className="font-semibold mr-1">KİŞİSEL E-MAIL</span>
                   tnhnipek@gmail.com
                 </p>
               </div>
@@ -69,7 +66,7 @@ const Body = () => {
         <Button />
       </div>
     </main>
-  )
+  );
 };
 
 export default Body;
--- home/app/layout.tsx
+++ home/app/layout.tsx
@@ -1,103 +1,104 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Chakra_Petch } from 'next/font/google'
-import Navbar from './_components/Navigation/Navbar'
-import { Suspense } from 'react';
-import Loading from './_components/Loading/Loading';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Chakra_Petch } from "next/font/google";
+import Navbar from "./_components/Navigation/Navbar";
+import { Suspense } from "react";
+import Loading from "./_components/Loading/Loading";
 
 const chakra = Chakra_Petch({
-  subsets: ['latin'],
-  weight: ['400'],
-  style: ['normal'],
-})
+  subsets: ["latin"],
+  weight: ["400"],
+  style: ["normal"],
+});
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://tunahanipek.com'),
-  title: 'Tunahan İPEK',
-  description: 'My personal website.',
+  metadataBase: new URL("https://tunahanipek.com"),
+  title: "Tunahan İPEK",
+  description: "My personal website.",
   openGraph: {
-    type: 'profile',
-    locale: 'en_US',
-    url: 'https://tunahanipek.com',
-    emails: ['tnhnipek@gmail.com', 'tunahan@rubiklabs.com'],
-    phoneNumbers: ['+905416064488'],
+    type: "profile",
+    locale: "en_US",
+    url: "https://tunahanipek.com",
+    emails: ["tnhnipek@gmail.com", "tunahan@rubiklabs.com"],
+    phoneNumbers: ["+905416064488"],
     images: [
       {
-        url: '/tunahanipek.jpg',
+        url: "/tunahanipek.jpg",
         width: 512,
         height: 512,
-        alt: 'Tunahan İPEK',
+        alt: "Tunahan İPEK",
       },
     ],
-    title: 'Tunahan İPEK',
-    description: 'My personal website.',
-    countryName: 'Turkey',
-    siteName: 'Tunahan İPEK',
+    title: "Tunahan İPEK",
+    description: "My personal website.",
+    countryName: "Turkey",
+    siteName: "Tunahan İPEK",
     ttl: 60 * 60 * 24 * 7,
-    alternateLocale: ['tr_TR'],
+    alternateLocale: ["tr_TR"],
   },
   twitter: {
-    site: 'https://tunahanipek.com',
-    title: 'Tunahan İPEK',
-    description: 'My personal website.',
+    site: "https://tunahanipek.com",
+    title: "Tunahan İPEK",
+    description: "My personal website.",
     images: [
       {
-        url: '/tunahanipek.jpg',
+        url: "/tunahanipek.jpg",
         width: 512,
         height: 512,
-        alt: 'Tunahan İPEK',
-        username: '@tunahanipek',
+        alt: "Tunahan İPEK",
+        username: "@tunahanipek",
       },
     ],
-    siteId: 'https://tunahanipek.com',
+    siteId: "https://tunahanipek.com",
   },
-  category: 'Personal Website',
+  category: "Personal Website",
   authors: [
     {
-      name: 'Tunahan İPEK',
-      url: 'https://tunahanipek.com',
+      name: "Tunahan İPEK",
+      url: "https://tunahanipek.com",
     },
   ],
-  themeColor: '#eba0bf',
-  creator: 'Tunahan İPEK',
-  publisher: 'Tunahan İPEK',
+  themeColor: "#eba0bf",
+  creator: "Tunahan İPEK",
+  publisher: "Tunahan İPEK",
   keywords: [
-    'Tunahan İPEK',
-    'Tunahan',
-    'İPEK',
-    'tnhnipek',
-    'tunahanipek',
-    'tunahan',
-    'ipek',
-    'tnhn',
-    'tnhnipek.com',
-    'tunahanipek.com',
-    'tunahanipekblog',
-    'blog',
-    'personal website',
-    'personal blog',
-    'personal',
-    'website',
-    'ipek tunahan',
+    "Tunahan İPEK",
+    "Tunahan",
+    "İPEK",
+    "tnhnipek",
+    "tunahanipek",
+    "tunahan",
+    "ipek",
+    "tnhn",
+    "tnhnipek.com",
+    "tunahanipek.com",
+    "tunahanipekblog",
+    "blog",
+    "personal website",
+    "personal blog",
+    "personal",
+    "website",
+    "ipek tunahan",
   ],
-  robots: 'index, follow',
+  robots: "index, follow",
   icons: [
     {
-      url: '/favicon.ico',
-      sizes: '16x16',
-      username: '@tunahanipek',
+      url: "/favicon.ico",
+      sizes: "16x16",
+      username: "@tunahanipek",
     },
   ],
-  generator: 'Tunahan İPEK',
+  generator: "Tunahan İPEK",
   appleWebApp: {
-    statusBarStyle: 'black-translucent',
-    title: 'Tunahan İPEK',
+    statusBarStyle: "black-translucent",
+    title: "Tunahan İPEK",
     startupImage: {
-      url: '/tunahanipek.jpg',
-      media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+      url: "/tunahanipek.jpg",
+      media:
+        "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
     },
   },
-  colorScheme: 'light',
+  colorScheme: "light",
   formatDetection: {
     url: true,
     email: true,
@@ -105,21 +106,21 @@ export const metadata: Metadata = {
     address: true,
     date: true,
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={chakra.className}>
+      <body className={chakra.className + " min-h-screen"}>
         <Suspense fallback={<Loading />}>
-          < Navbar />
+          <Navbar />
           {children}
         </Suspense>
       </body>
-    </html >
-  )
+    </html>
+  );
 }
