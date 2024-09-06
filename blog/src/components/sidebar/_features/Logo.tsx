df51f8e791a212ad9821fdeee4fc79019c36d748
Tunahan İPEK <tnhnipek@gmail.com>
2024-09-06 14:53:32 +0300

    logo

--- blog/src/components/sidebar/_features/Logo.tsx
+++ blog/src/components/sidebar/_features/Logo.tsx
@@ -1,6 +1,15 @@
+'use client';
+
+import { useRouter } from '@/navigation';
+
 const LogoFeature = () => {
+  const router = useRouter();
+
   return (
-    <div className="self-start h-auto text-center max-sm:mr-10">
+    <div
+      className="self-start h-auto text-center max-sm:mr-10"
+      onClick={() => router.push('/')}
+    >
       <button className="transition duration-500 ease-in-out transform mr-44 animate-skew hover:-translate-y-1 hover:-translate-x-2 hover:scale-110 hover:scale-y-125">
         <div className="relative box">
           <p className="text-green-500 text text1">tunahan</p>
