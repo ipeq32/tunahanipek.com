47d68b80a7e9549e2685bdde70b00f58d2c6b9f3
Tunahan İPEK <tnhnipek@gmail.com>
2024-09-20 22:14:48 +0300

    error page

--- blog/src/app/[locale]/error.tsx
+++ blog/src/app/[locale]/error.tsx
@@ -18,17 +18,19 @@ export default function Error({
   }, [error]);
 
   return (
-    <main className="container grid min-h-screen place-content-center space-y-5 text-center bg-sky-50 dark:bg-primary/90">
-      <h1 className="text-3xl font-semibold">{t('title')}</h1>
-      <p>{error.message}</p>
-      <section className="space-x-8">
-        <Button onClick={() => reset()} className="font-semibold">
-          {t('Button.try')}
-        </Button>
-        <Button asChild variant="secondary" className="font-semibold">
-          <a href="/">{t('Button.back')}</a>
-        </Button>
-      </section>
+    <main className="w-full grid min-h-screen place-content-center space-y-5 text-center bg-sky-50 dark:bg-primary/90">
+      <div className="container">
+        <h1 className="text-3xl font-semibold">{t('title')}</h1>
+        <p>{error.message}</p>
+        <section className="space-x-8">
+          <Button onClick={() => reset()} className="font-semibold">
+            {t('Button.try')}
+          </Button>
+          <Button asChild variant="secondary" className="font-semibold">
+            <a href="/">{t('Button.back')}</a>
+          </Button>
+        </section>
+      </div>
     </main>
   );
 }
