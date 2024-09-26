37ae62fea11932f2ec78d328d51c57420154a321
Tunahan İPEK <tnhnipek@gmail.com>
2024-09-26 13:00:28 +0300

    undefined name property

--- blog/src/app/[locale]/(main)/blog/_features/Blogs.tsx
+++ blog/src/app/[locale]/(main)/blog/_features/Blogs.tsx
@@ -40,7 +40,7 @@ const BlogsFeature = ({ data }: BlogFeatureProps) => {
             />
             <figcaption className="flex justify-between gap-2">
               <span className="text-sm line-clamp-1">
-                {blog?.author.name} -{' '}
+                {blog.author ? blog?.author.name : 'Anonim'} -{' '}
                 {blog.author.role === 'SUPER_ADMIN' ? 'Yönetici' : 'Yazar'}
               </span>
               <span className="text-xs line-clamp-1 text-opacity-40">
