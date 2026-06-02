3a9b8fcfc4def7592a9484b07e0afcccb093b517
Tunahan İPEK <tnhnipek@gmail.com>
2026-06-02 12:14:10 +0300

    Add Contact and FAQ pages with HeaderTemplate integration
    - Created new Contact page with Turkish title and description using HeaderTemplate.
    - Added FAQ page featuring Turkish title and description, also utilizing HeaderTemplate for consistency in layout.

---
+++ blog/src/app/[locale]/(main)/contact/page.tsx
@@ -0,0 +1,12 @@
+import HeaderTemplate from '@/components/templates/HeaderTemplate';
+
+function Page() {
+  return (
+    <HeaderTemplate
+      title="İletişim"
+      description="İletişim sayfası açıklama kısmı."
+    />
+  );
+}
+
+export default Page;
---
+++ blog/src/app/[locale]/(main)/faq/page.tsx
@@ -0,0 +1,12 @@
+import HeaderTemplate from '@/components/templates/HeaderTemplate';
+
+function Page() {
+  return (
+    <HeaderTemplate
+      title="Sıkça Sorulan Sorular"
+      description="SSS sayfası açıklama kısmı."
+    />
+  );
+}
+
+export default Page;
